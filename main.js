ffc22a73570f71ca78f16406e88c9e007379fe19
Lautaro Avaro <lautaroavaro1@gmail.com>
2023-06-10 19:10:10 -0300

    Vue creado y boton me gusta

--- main.js
+++ main.js
@@ -1,40 +1,19 @@
-var likes = 200;
-
-function sumarLike(){
-  likes++
-  document.getElementById("likes").innerHTML = likes;
-  document.getElementById("megusta").disabled = true;
-}
-
-var seguir = false;
-
-function cambiarSeguir() {
-  var btnSeguir = document.getElementById("btnSeguir");
-  if (seguir){
-    seguir = false;
-    btnSeguir.innerHTML = "Seguir"
-  }
-  else{
-    seguir = true;
-    btnSeguir.innerHTML = "Dejar de seguir" 
+const app = Vue.createApp({
+  data(){
+    return{
+      botonMegusta: 'Me Gusta',
+      cantLikes: 200,
+      like: false,
+    }
+  },
+methods: {
+  sumarLike(){
+    this.like = true;
+    this.cantLikes +=1;
   }
 }
 
-function comentar(){
-  var usuario = document.getElementById("usuario").value;
-  var comentario = document.getElementById("comentario").value;
-  var error = document.getElementById("mostrarError");
-
-  if (usuario === ""){
-    error.innerHTML = "Debe ingresar su usuario";
-  }
-  else if (comentario === ""){
-    error.innerHTML = "Debe ingresar un comentario";
-  }
-
-}
+})
 
 
-
-
-
+const appMontada = app.mount('#app');
