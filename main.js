f069279ecb40fd43fb062a2e157f7b0da8714ac6
Lautaro Avaro <lautaroavaro1@gmail.com>
2023-06-12 02:44:36 -0300

    caja de comentarios y boton eliminar

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const app = Vue.createApp({
       cantLikes: 200,
       like: false,
       botonSeguir: false,
+      ingresoUsuario: '',
+      ingresoComentario: '',
+      errorUsuario: false,
+      errorComentario: false,
+      comentarios: []
     }
   },
 methods: {
@@ -14,7 +19,35 @@ methods: {
   },
   siguiendo(){
     this.botonSeguir = !this.botonSeguir;
+  },
+  agregComentario(){
+
+    if(!this.ingresoUsuario){
+      this.errorUsuario = true;
+    }
+    else{
+      this.errorUsuario = false;
+    }
+
+    if(!this.ingresoComentario){
+      this.errorComentario = true;
+    }
+    else{
+      this.errorComentario = false;
+    }
+
+    if(this.ingresoUsuario && this.ingresoComentario){
+      this.comentarios.push({
+        ingresoUsuario: this.ingresoUsuario,
+        ingresoComentario: this.ingresoComentario
+      })
+    }
+     
+  },
+  borrarComentario(borrar){
+    this.comentarios.splice(borrar, 1);
   }
+
 }
 
 })
